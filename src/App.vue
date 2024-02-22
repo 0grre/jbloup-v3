965a6a8c9f8db83e29134bd20a65690199a67504
ogrre <jbloup@hotmail.fr>
2024-02-22 00:40:02 -0500

    feat(html): update project to render site only with html

--- src/App.vue
+++ src/App.vue
@@ -1,84 +1,67 @@
 <script setup>
-import octokit from "../services/github.js";
-import {ref, onMounted, onUnmounted, nextTick, onBeforeMount} from 'vue';
 import Header from "./components/Header.vue";
 import Footer from "./components/Footer.vue";
 import About from "./components/About.vue";
 import Experiences from "./components/Experiences.vue";
 import data from "../services/french/resume.js"
-
-const about = ref(null);
-const experiences = ref(null);
-const projects = ref(null);
-const currentSection = ref('');
-
-// let data = ref(null);
-// let data
-//
-// async function fetchData() {
-//   try {
-//     const response = await octokit.request("GET /gists/ae63e34c62d41c9f5e053eb6661ff42d", {
-//       gist_id: 'ae63e34c62d41c9f5e053eb6661ff42d',
-//       headers: {
-//         'X-GitHub-Api-Version': '2022-11-28',
-//         'accept': 'application/vnd.github.v3+json'
-//       }
-//     });
-//
-//     const gist = await response.data;
-//     const fileName = Object.keys(gist.files)[0];
-//     data = JSON.parse(gist.files[fileName].content);
-//     console.log(data.basics)
-//
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
-
-onMounted( () => {
-  // fetchData();
-
-  console.log(data)
-  nextTick(() => {
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          currentSection.value = entry.target.id;
-          console.log(currentSection.value)
-        }
-      });
-    }, {
-      rootMargin: '0px',
-      threshold: 0.5
-    });
-
-    if (about.value) observer.observe(about.value);
-    if (experiences.value) observer.observe(experiences.value);
-    if (projects.value) observer.observe(projects.value);
-  });
-});
-
-onUnmounted(() => {
-  // observer.disconnect();
-});
-
-
+import Projects from "./components/Projects.vue";
 </script>
 
-
 <template>
-  <section class="mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-museo md:px-12 md:py-20 lg:px-24 lg:py-0">
-    <div class="lg:flex lg:justify-between lg:gap-4">
+  <header>
+    <nav>
+      <center>
+        <details>
+          <summary>Navigation</summary>
+          <table>
+            <thead>
+            <tr>
+              <td></td>
+              <td width="150px">
+                <ul>
+                  <li>
+                    <a href="#about">About</a>
+                  </li>
+                  <li>
+                    <a href="#projects">Projects</a>
+                  </li>
+                  <li>
+                    <a href="#experience">Experiences</a>
+                  </li>
+                </ul>
+              </td>
+              <td></td>
+            </tr>
+            </thead>
+          </table>
+        </details>
+      </center>
+    </nav>
+  </header>
+  <hr>
+  <table>
+    <tbody>
+    <tr>
       <Header :basics="data.basics"/>
-      <main id="content" class="pt-24 lg:w-1/2 lg:py-24">
-        <About :basics="data.basics"/>
-        <Experiences :work="data.work"/>
-        <section ref="projects" id="projects"
-                 :class="{ active: currentSection === 'projects' } + 'mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24'"
-                 aria-label="Projects">
-        </section>
-        <Footer/>
-      </main>
-    </div>
-  </section>
+      <About :basics="data.basics"/>
+    </tr>
+    <tr>
+      <td width="40%">
+
+      </td>
+      <Experiences :work="data.work"/>
+    </tr>
+    <tr>
+      <td width="40%">
+      </td>
+      <td>
+        <hr>
+        <Projects :projects="data.projects"/>
+      </td>
+    </tr>
+    </tbody>
+    <tfoot>
+    <Footer/>
+    </tfoot>
+  </table>
 </template>
